debeab94e519d293ef64955165064fb2cd476730
VeIskar <mlewandowski380@gmail.com>
2024-11-07 23:59:55 +0100

    add: plots for all combinations per sensors

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -46,7 +46,91 @@ participants_df = category_df = df.query("label == 'bench'").sort_values("partic
 
 
 fig, ax = plt.subplots()
-participants_df.groupby(["category"])["acc_y"].plot()
+participants_df.groupby(["participant"])["acc_y"].plot()
 ax.set_ylabel("acc_y")
 ax.set_xlabel("samples")
 plt.legend()
+
+
+#multiple plot axis
+
+label = "squat"
+
+participant = "A" 
+#all_axis_df = df.query(f"label == '{participant}'").reset_index()
+all_axis_df = df.query(f"label == '{label}'").query(f"label == '{participant}'").reset_index()
+
+
+#data for squatting exercise of participant A in all the 3 axis
+#basically movement pattern of the person doing the exercise is displayed through plots
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
+
+
+#loop for plots with combinations per sensor
+
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for lbl in labels:
+    for prt in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"label == '{participant}'")
+            .reset_index()
+
+        )
+
+        if len(all_axis_df)>0:
+
+            fig, ax = plt.subplots()
+            all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+            ax.set_ylabel("acc_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
+    
+
+
+#for gyroscope data
+for lbl in labels:
+    for prt in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"label == '{participant}'")
+            .reset_index()
+
+        )
+
+        if len(all_axis_df)>0:
+
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax)
+            ax.set_ylabel("gyr_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
+
+
+
+#combining into one figure
+
+label = "row"
+participant = "A"
+combined_plot_df = (
+    df.query(f"label == '{label}'")
+    .query(f"label == '{participant}'")
+    .reset_index(drop = True)
+)
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[0])
+combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol =3, fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol =3, fancybox=True, shadow=True)
+ax[1].set_xlabel("samples")
