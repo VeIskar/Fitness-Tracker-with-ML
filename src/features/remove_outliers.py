c74ee236a8cbdcf63a40a1974a02db69440edd40
VeIskar <mlewandowski380@gmail.com>
2024-12-25 00:00:01 +0100

    add: Chauvenet criterion

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -85,7 +85,7 @@ def plot_binary_outliers(dataset, col, outlier_col, reset_index):
     )
     plt.show()
 
-
+###DISTRIBUTION BASED METHODS:
 
 #marking outliers with IQR (Interquartile range)
 def mark_outliers_iqr(dataset, col):
@@ -120,4 +120,74 @@ col = "acc_x"
 dataset = mark_outliers_iqr(df, col)
 #dataset.head()
 
-plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"outlier",reset_index=True)
+plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"_outlier",reset_index=True)
+
+#loop over all columns
+for col in outlier_cols:
+    dataset = mark_outliers_iqr(df, col)
+    plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"_outlier",reset_index=True)
+
+
+#Chauvenet criterion
+#According to Chauvenet’s criterion we reject a measurement (outlier) 
+#from a dataset of size N when it’s probability of observation is less than 1/2N
+#a generalization is to replace the value 2 with a parameter C.
+
+#checking for normal distribution
+#look at either histogram or boxplot:
+#Histogram — Do you see a bell shaped curve / Boxplot — Is the box symmetrical
+
+#histograms:
+df[outlier_cols[:3]+["label"]].plot.hist(by="label", 
+                                       figsize =(20,10), layout =(3,3))
+
+df[outlier_cols[3:]+["label"]].plot.hist(by="label", 
+                                       figsize =(20,10), layout =(3,3))
+
+
+
+def mark_outliers_chauvenet(dataset, col, C=2):
+    """Finds outliers in the specified column of datatable and adds a binary column with
+    the same name extended with '_outlier' that expresses the result per data point.
+    
+    Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/Chapter3/OutlierDetection.py
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+        C (int, optional): Degree of certainty for the identification of outliers given the assumption 
+                           of a normal distribution, typicaly between 1 - 10. Defaults to 2.
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column 
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+    # Compute the mean and standard deviation.
+    mean = dataset[col].mean()
+    std = dataset[col].std()
+    N = len(dataset.index)
+    criterion = 1.0 / (C * N)
+
+    # Consider the deviation for the data points.
+    deviation = abs(dataset[col] - mean) / std
+
+    # Express the upper and lower bounds.
+    low = -deviation / math.sqrt(C)
+    high = deviation / math.sqrt(C)
+    prob = []
+    mask = []
+
+    # Pass all rows in the dataset.
+    for i in range(0, len(dataset.index)):
+        # Determine the probability of observing the point
+        prob.append(
+            1.0 - 0.5 * (scipy.special.erf(high[i]) - scipy.special.erf(low[i]))
+        )
+        # And mark as an outlier when the probability is below our criterion.
+        mask.append(prob[i] < criterion)
+    dataset[col + "_outlier"] = mask
+    return dataset
+
+
