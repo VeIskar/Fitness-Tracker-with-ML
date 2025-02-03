bd6ac9589a86cbd955f972ad3e0276e1ce1e8f32
VeIskar <mlewandowski380@gmail.com>
2025-02-03 23:58:14 +0100

    add: butterworth low-pass filter

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -48,3 +48,32 @@ dur_df.iloc[0]/5
 dur_df.iloc[1]/10
 
 
+#butterworth low-pass filter
+
+df_lowpas = df.copy()
+lowpass = LowPassFilter()
+
+fs = 1000/200 #five instances per second
+cutoff = 1.3 #(0.4)low number smooth data high(4) -> close to raw data 
+
+df_lowpas = lowpass.low_pass_filter(df_lowpas, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpas[df_lowpas["set"]==45]
+print(subset["label"][0])
+
+
+#data visualization
+fig, ax =plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+ax[0].plot(subset["acc_y"].reset_index(drop = True), label ="raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop = True), label ="butterworth filter")
+ax[0].legend(loc = "upper center", bbox_to_anchro=(0.5, 1.15), fancybox = True, shadow=True)
+ax[1].legend(loc = "upper center", bbox_to_anchro=(0.5, 1.15), fancybox = True, shadow=True)
+
+
+for col in predictor_cols:
+    df_lowpas = LowPassFilter.low_pass_filter(df_lowpas, col , fs, cutoff, order=5)
+    #overwriting original column
+    df_lowpas[col] = df_lowpas[col+ "_lowpass"]
+    del df_lowpas[col+ "_lowpass"]
+
+
